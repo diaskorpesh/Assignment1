dfe2d1c5b0de575323f31d7be07315312ec21c97
diaskorpesh <118056718+diaskorpesh@users.noreply.github.com>
2025-03-30 17:23:37 +0500

    Update Main.java

--- src/main/java/task7/Main.java
+++ src/main/java/task7/Main.java
@@ -1,4 +1,19 @@
-package task7;
+public class Permutations {
 
-public class Main {
+    public static void generatePermutations(String str, String chosen) {
+        if (str.length() == 0) {
+            System.out.println(chosen);
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                String remaining = str.substring(0, i) + str.substring(i + 1);
+                generatePermutations(remaining, chosen + c);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String input = "IOX";
+        generatePermutations(input, "");
+    }
 }
